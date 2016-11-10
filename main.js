d66f4034847d48f078909078ca7d2b1833950dbf
ashintr <ashintr@ncsu.edu>
2016-11-10 15:02:48 -0500

    Commit

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var redisIp = fs.readFileSync('redisServer.txt', 'utf-8');
 var client = redis.createClient(6379, redisIp, {})
 var count = 0;
 
-var client = require('./store.js');
+// var client = require('./store.js');
 // client.set("feature", "off");
 
 app.get('/', function(req, res){
@@ -17,82 +17,80 @@ app.get('/', function(req, res){
 });
 
 app.get('/set', function(req, res){
-	// client.get("feature", function(err, value){
-	// 	if(value == "on"){
-	// 		res.send('Key set!');			
-	// 	}
-	// 	else{
-	// 		res.send('Not allowed to set key');
-	// 	}
-	// });
-	console.log('/set');
-	client.getFeature(function(value){
-		console.log("value is ", value);
-		if(value == "on")
-		{
+	client.get("feature", function(err, value){
+		if(value == "on"){
 			res.send('Key set!');			
 		}
-		else
-		{
+		else{
 			res.send('Not allowed to set key');
-		}	
+		}
 	});
+	// console.log('/set');
+	// client.getFeature(function(value){
+	// 	console.log("value is ", value);
+	// 	if(value == "on")
+	// 	{
+	// 		res.send('Key set!');			
+	// 	}
+	// 	else
+	// 	{
+	// 		res.send('Not allowed to set key');
+	// 	}	
+	// });
 	
 });
 
 
-// app.get('/get', function(req, res){
-// 	count = count + 1;
-// 	if(count == 3)
-// 	{
-// 		res.send("Get")
-// 		client.set("alert", true);
-// 	}
-// 	else
-// 	{
-// 		client.set("alert", false);
-// 		res.send("Alert!");
-// 	}
-// });
+app.get('/get', function(req, res){
+	count = count + 1;
+	if(count == 3)
+	{
+		res.send("Get")
+		client.set("alert", true);
+	}
+	else
+	{
+		client.set("alert", false);
+		res.send("Alert!");
+	}
+});
 
 if(true){
 	console.log("True");
 }
 
+// app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+//    // console.log(req.body) // form fields
+//    // console.log(req.files) // form files
+// 	if(req.body.size > 50000)
+// 		client.set("alert",true); // form fields
+// 	else
+// 		client.set("alert", false);
+// 	   if( req.files.image )
+// 	   {
+// 		   fs.readFile( req.files.image.path, function (err, data) {
+// 		  		if (err) throw err;
+// 		  		var img = new Buffer(data).toString('base64');
+// 		  		// console.log(img);
+// 		  		client.lpush("imglist", img, function(err, value){
+// 		  		})
+// 			});
+// 		}
 
+//    res.status(204).end()
+// }]);
 
-app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
-   // console.log(req.body) // form fields
-   // console.log(req.files) // form files
-	if(req.body.size > 50000)
-		client.set("alert",true); // form fields
-	else
-		client.set("alert", false);
-	   if( req.files.image )
-	   {
-		   fs.readFile( req.files.image.path, function (err, data) {
-		  		if (err) throw err;
-		  		var img = new Buffer(data).toString('base64');
-		  		// console.log(img);
-		  		client.lpush("imglist", img, function(err, value){
-		  		})
-			});
-		}
-
-   res.status(204).end()
-}]);
-
-app.get('/meow', function(req, res) {
-	{
-		client.lpop("imglist", function(err,imagedata){
-			// console.log(imagedata.length);
-			// client.set("imgLength", imagedata.length);
-			res.writeHead(200, {'content-type':'text/html'});
-			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
-			res.end();
-		});
-	}
-})
+// app.get('/meow', function(req, res) {
+// 	{
+// 		client.lpop("imglist", function(err,imagedata){
+// 			// console.log(imagedata.length);
+// 			// client.set("imgLength", imagedata.length);
+// 			res.writeHead(200, {'content-type':'text/html'});
+// 			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+// 			res.end();
+// 		});
+// 	}
+// })
 
 var server = app.listen(3000, function(){
 	var host = server.address().address;
