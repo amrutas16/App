9312e599ff4e111885fb0944d1617b53121fd462
ashintr <ashintr@ncsu.edu>
2016-11-10 15:23:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -41,56 +41,56 @@ app.get('/set', function(req, res){
 });
 
 
-app.get('/get', function(req, res){
-	count = count + 1;
-	if(count == 3)
-	{
-		res.send("Get")
-		client.set("alert", true);
-	}
-	else
-	{
-		client.set("alert", false);
-		res.send("Alert!");
-	}
-});
+// app.get('/get', function(req, res){
+// 	count = count + 1;
+// 	if(count == 3)
+// 	{
+// 		res.send("Get")
+// 		client.set("alert", true);
+// 	}
+// 	else
+// 	{
+// 		client.set("alert", false);
+// 		res.send("Alert!");
+// 	}
+// });
 
 if(true){
 	console.log("True");
 }
 
-// app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
-//    // console.log(req.body) // form fields
-//    // console.log(req.files) // form files
-// 	if(req.body.size > 50000)
-// 		client.set("alert",true); // form fields
-// 	else
-// 		client.set("alert", false);
-// 	   if( req.files.image )
-// 	   {
-// 		   fs.readFile( req.files.image.path, function (err, data) {
-// 		  		if (err) throw err;
-// 		  		var img = new Buffer(data).toString('base64');
-// 		  		// console.log(img);
-// 		  		client.lpush("imglist", img, function(err, value){
-// 		  		})
-// 			});
-// 		}
+app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+   // console.log(req.body) // form fields
+   // console.log(req.files) // form files
+	if(req.body.size > 50000)
+		client.set("alert",true); // form fields
+	else
+		client.set("alert",false);
+   if( req.files.image )
+   {
+	   fs.readFile( req.files.image.path, function (err, data) {
+	  		if (err) throw err;
+	  		var img = new Buffer(data).toString('base64');
+	  		// console.log(img);
+	  		client.lpush("imglist", img, function(err, value){
+	  		})
+		});
+	}
 
-//    res.status(204).end()
-// }]);
+   res.status(204).end()
+}]);
 
-// app.get('/meow', function(req, res) {
-// 	{
-// 		client.lpop("imglist", function(err,imagedata){
-// 			// console.log(imagedata.length);
-// 			// client.set("imgLength", imagedata.length);
-// 			res.writeHead(200, {'content-type':'text/html'});
-// 			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
-// 			res.end();
-// 		});
-// 	}
-// })
+app.get('/meow', function(req, res) {
+	{
+		client.lpop("imglist", function(err,imagedata){
+			// console.log(imagedata.length);
+			// client.set("imgLength", imagedata.length);
+			res.writeHead(200, {'content-type':'text/html'});
+			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+			res.end();
+		});
+	}
+})
 
 var server = app.listen(3000, function(){
 	var host = server.address().address;
