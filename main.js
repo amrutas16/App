83e1b25ef8cd124e4e62aee12db435cbe5f2825f
ashintr <ashintr@ncsu.edu>
2016-11-10 21:10:56 -0500

    Final commit

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ var app = express();
 var fs = require('fs');
 var multer  = require('multer');
 var redisIp = fs.readFileSync('redisServer.txt', 'utf-8');
-var client = redis.createClient(6379, redisIp, {})
+// var client = redis.createClient(6379, redisIp, {})
+var client = redis.createClient(6379, '127.0.0.1', {})
 var count = 0;
 
 app.get('/', function(req, res){
@@ -17,26 +18,12 @@ app.get('/set', function(req, res){
 			res.send('Key set!');			
 		}
 		
+		
 		else{
 			res.send('Not allowed to set key');
 		}
 	});
 });
-
-
-// app.get('/get', function(req, res){
-// 	count = count + 1;
-// 	if(count == 3)
-// 	{
-// 		res.send("Get")
-// 		client.set("alert", true);
-// 	}
-// 	else
-// 	{
-// 		client.set("alert", false);
-// 		res.send("Alert!");
-// 	}
-// });
 
 app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
    // console.log(req.body) // form fields
@@ -47,10 +34,10 @@ app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
 	   fs.readFile( req.files.image.path, function (err, data) {
 	  		if (err) {throw err};
 	  		var img = new Buffer(data).toString('base64');
-	  		if(img.length > 50000)
-				client.set("alert",true); // form fields
-			else
-				client.set("alert",false);
+	  // 		if(img.length > 50000)
+			// 	client.set("alert",true); // form fields
+			// else
+			// 	client.set("alert",false);
 	  		client.lpush("imglist", img, function(err, value){
 	  		})
 		});
@@ -62,9 +49,18 @@ app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
 app.get('/meow', function(req, res) {
 	{
 		client.lpop("imglist", function(err,imagedata){
-			res.writeHead(200, {'content-type':'text/html'});
-			res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
-			res.end();
+			// if(imagedata.length > 50000)
+			// {
+			// 	client.set("alert",true);
+			// 	res.send("Image too large to display. Route to production here onwards...");
+			// }
+			// else
+			// {
+				res.writeHead(200, {'content-type':'text/html'});
+				res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+				res.end();	
+			// }
+			
 		});
 	}
 })
